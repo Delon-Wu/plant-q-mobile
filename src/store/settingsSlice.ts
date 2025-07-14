860dd63309676c8ae7887b31c59d74a243c354f0
JinLong Wu <2474323762@qq.com>
2025-07-14 18:54:04 +0800

    feat: 修改HOST配置，移动端才使用env的host

--- src/store/settingsSlice.ts
+++ src/store/settingsSlice.ts
@@ -1,11 +1,12 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Platform } from 'react-native';
 
 interface SettingsState {
   host: string;
 }
 
 const initialState: SettingsState = {
-  host: process.env.NODE_ENV === 'development' ? process.env.EXPO_PUBLIC_DEV_API_HOST ?? '' : '',
+  host: process.env.NODE_ENV === 'development' && Platform.OS !== "web" ? process.env.EXPO_PUBLIC_DEV_API_HOST ?? '' : '',
 };
 
 const settingsSlice = createSlice({
