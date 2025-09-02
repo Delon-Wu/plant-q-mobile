5fe7302f0502f24289626878067ec6b39dfaab2f
JinLong Wu <2474323762@qq.com>
2025-09-02 15:43:22 +0800

    feat: 优化打包

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,25 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
-  
+
 const config = getDefaultConfig(__dirname);
 
-// Add SVG transformer
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve('react-native-svg-transformer'),
-};
+// Add SVG transformer with error handling
+try {
+  const svgTransformerPath = require.resolve('react-native-svg-transformer');
+  
+  config.transformer = {
+    ...config.transformer,
+    babelTransformerPath: svgTransformerPath,
+  };
 
-config.resolver = {
-  ...config.resolver,
-  assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
-  sourceExts: [...config.resolver.sourceExts, 'svg'],
-};
+  config.resolver = {
+    ...config.resolver,
+    assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...config.resolver.sourceExts, 'svg'],
+  };
+} catch (_error) {
+  console.warn('react-native-svg-transformer not found, SVG support will be limited');
+  console.warn('Run: npm install react-native-svg-transformer');
+}
   
 module.exports = withNativeWind(config, { input: './global.css' });
